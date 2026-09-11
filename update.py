e2af5687429f87bde9ba6e945b06bad039c699d7
jazc2002-cloud <jazc2002@gmail.com>
2026-09-11 02:00:32 -0600

    Enhance regex for multiline HTML replacement
    Added re.DOTALL flag to regex substitution for multiline support.

--- update.py
+++ update.py
@@ -7,10 +7,12 @@ with open('index.html','r',encoding='utf-8') as f:
 ahora = datetime.now().strftime('%d %b %Y %H:%M CDMX')
 nueva_nube = f'✅ V63 AUTO {ahora} - NUBE LIMPIA - FORMATO COMPLETO'
 
-html = re.sub(r'<div id="nube">.*?</div>', f'<div id="nube">{nueva_nube}</div>', html)
+html = re.sub(r'<div id="nube">.*?</div>', f'<div id="nube">{nueva_nube}</div>', html, flags=re.DOTALL)
 
 with open('index.html','w',encoding='utf-8') as f:
     f.write(html)
 
 with open('last_update.txt','w') as f:
     f.write(nueva_nube)
+
+print("Listo")
