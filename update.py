b7acf39aed59b93a175a65b7b053f70a1307159d
jazc2002-cloud <jazc2002@gmail.com>
2026-09-11 02:05:57 -0600

    Update version string in update.py

--- update.py
+++ update.py
@@ -5,7 +5,7 @@ with open('index.html','r',encoding='utf-8') as f:
     html = f.read()
 
 ahora = datetime.now().strftime('%d %b %Y %H:%M CDMX')
-nueva_nube = f'✅ V63 AUTO {ahora} - NUBE LIMPIA - FORMATO COMPLETO'
+nueva_nube = f'✅ V62 RESTAURADO {ahora} - NUBE LIMPIA - FORMATO COMPLETO - AUTO'
 
 html = re.sub(r'<div id="nube">.*?</div>', f'<div id="nube">{nueva_nube}</div>', html, flags=re.DOTALL)
 
@@ -14,5 +14,3 @@ with open('index.html','w',encoding='utf-8') as f:
 
 with open('last_update.txt','w') as f:
     f.write(nueva_nube)
-
-print("Listo")
